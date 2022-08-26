dbeca88863a0345d6e3da9e84e4346c229d1992d
Brandon They <theyb5220@gmail.com>
2022-08-26 11:42:16 -0500

    Fetched data from api based on city name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
+import { useState, useEffect } from 'react';
 function App() {
+
+  const [weatherInfo, setWeatherInfo] = useState();
+
+  const apiKey = "69d0a94339a676369beaced8ff6ac0d7";
+  
+  const getWeatherInfo = (searchTerm) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}`)
+    .then((respone) => {
+      return respone.json();
+    })
+    .then((data) => {
+      setWeatherInfo(data);
+      console.log(data)
+    })
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const searchTerm = event.target[0].value;
     console.log(searchTerm);
+    getWeatherInfo(searchTerm)
   }
 
   return (
